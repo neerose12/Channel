1ca5ebb7e28370992b13577f92761f2f9b336678
Niroj Maharjan <niroj23@gmail.com>
2018-05-28 11:56:00 +0545

    change fragment import

--- app/src/main/java/com/wlink/nettv/nettvlivetv/base/BaseFragment.java
+++ app/src/main/java/com/wlink/nettv/nettvlivetv/base/BaseFragment.java
@@ -1,10 +1,10 @@
 package com.wlink.nettv.nettvlivetv.base;
 
+import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.RecyclerView;
 import android.util.TypedValue;
