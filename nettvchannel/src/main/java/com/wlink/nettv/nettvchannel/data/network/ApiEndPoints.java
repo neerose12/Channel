cd9c85999c415e2b077f3ce79cd3ec4dc7511e44
Niroj Maharjan <niroj.mahrjan@gmail.com>
2018-07-11 15:39:39 +0545

    url changed

--- nettvchannel/src/main/java/com/wlink/nettv/nettvchannel/data/network/ApiEndPoints.java
+++ nettvchannel/src/main/java/com/wlink/nettv/nettvchannel/data/network/ApiEndPoints.java
@@ -3,5 +3,5 @@ package com.wlink.nettv.nettvchannel.data.network;
 public final class ApiEndPoints {
 
     public static final String BASE = "http://new.nettv.com.np/public/";
-    public static final String CHANNEL = BASE +"resellers/5/categories-channels-lsist";
+    public static final String CHANNEL = BASE +"resellers/5/categories-channels-list";
 }
