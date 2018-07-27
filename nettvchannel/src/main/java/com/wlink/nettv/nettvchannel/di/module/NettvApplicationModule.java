bc11e2818d3f2cbde580444ef0707917bdb07946
Niroj <niroj.mahrjan@gmail.com>
2018-07-27 16:50:38 +0545

    access token from shared preferecences added

--- nettvchannel/src/main/java/com/wlink/nettv/nettvchannel/di/module/NettvApplicationModule.java
+++ nettvchannel/src/main/java/com/wlink/nettv/nettvchannel/di/module/NettvApplicationModule.java
@@ -11,6 +11,7 @@ import com.wlink.nettv.nettvchannel.data.network.ApiHelper;
 import com.wlink.nettv.nettvchannel.data.network.AppApiHelper;
 import com.wlink.nettv.nettvchannel.di.ApiInfo;
 import com.wlink.nettv.nettvchannel.di.ApplicationContext;
+import com.wlink.nettv.nettvchannel.main.NettvApp;
 
 import javax.inject.Singleton;
 
@@ -53,7 +54,7 @@ public class NettvApplicationModule {
     @Provides
     @ApiInfo
     String provideApiKey() {
-        return "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjE3Y2U1MjVlNDIxYmNjMTQ4MDViMzZlYzAyZDk5NzhiNDY5NDIwYTExOWZjZmY4OWY0ZjNmNGZlOTNmNTAzYzExYjMzMDdhOWE1MDBkYTZiIn0.eyJhdWQiOiI2IiwianRpIjoiMTdjZTUyNWU0MjFiY2MxNDgwNWIzNmVjMDJkOTk3OGI0Njk0MjBhMTE5ZmNmZjg5ZjRmM2Y0ZmU5M2Y1MDNjMTFiMzMwN2E5YTUwMGRhNmIiLCJpYXQiOjE1MzI2MDA2MjYsIm5iZiI6MTUzMjYwMDYyNiwiZXhwIjoxNTY0MTM2NjI2LCJzdWIiOiI4Iiwic2NvcGVzIjpbInN1YnNjcmliZXIiXX0.MUALeeHs-Z6T8czSMKtrFr7kii2pf6cZQ-DOR-mesymaOUCt1PTLtduiqgkE8eh7MQLX_4R7p5ae0DtxUIapOOSUCzddjPI57h71iVeFRqXVVo4EQe8tQrJ6JWzfkc0JttqC6ImzKK3D1RR4OcLB_NnoZWWEwtXBo7LP7zVO_jLVLjg3NOau8Cri0pLmecT9aKaR5K1rHyXEinqWcaFxIuiu0yrMg7_ASGcngZydG__hSCCPNyOa-7imfLRDcXJ4T397x-aHXA7ABzQPXCCRta_NCMndUse_nmCWlRjlLLNXABD8-kMBXt73ueza-65PjNV0r6q3JKNmo2jDnSaaOvMjp6i0BN5KXzrQZyM8PZwaLOnrZs9wqWSGjOtfzUwQiuTo69MAuUvVrVxgpoo5WeJ0iFxYaUT0dpyj-g9lQeRE-zLYyp2gqZqXHW3KG2FTlYaRmycB4ahf71wfnoiPA6Y67-m8oBDn2MgUkE1J50oRR4bTzbQYtDqa3Ksepj8Pl2t_vcA0rk_G42rhBMkb5Lq2twV78OZzOlaqZ501XXNFCxk7-UTJEPQkv0KpXgJnwu63x1v64gmjRaEYCFGkqlarwHBGGISKLPVQBsF5Ng135pgli6i8TMpDDJ7bbD_KODRheR68Taptns_hJxznkGPOyxaIE19J8Qu7Z4jiZ04";
+        return "Bearer "+ NettvApp.accessToken;
     }
 
     @Provides
--- nettvchannel/src/main/java/com/wlink/nettv/nettvchannel/main/NettvApp.java
+++ nettvchannel/src/main/java/com/wlink/nettv/nettvchannel/main/NettvApp.java
@@ -1,7 +1,12 @@
 package com.wlink.nettv.nettvchannel.main;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.support.multidex.MultiDexApplication;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.androidnetworking.AndroidNetworking;
 import com.parse.Parse;
@@ -19,17 +24,26 @@ import javax.inject.Inject;
 import okhttp3.OkHttpClient;
 
 
-public class NettvApp extends MultiDexApplication{
+public class NettvApp extends MultiDexApplication {
 
     @Inject
     DataManager mDataManager;
-
+    Context mContext;
     private NettvApplicationComponent nettvApplicationComponent;
-
+    public static String accessToken;
     @Override
     public void onCreate() {
         super.onCreate();
-
+        this.mContext = getApplicationContext();
+        try {
+            Context myContext = createPackageContext("np.com.nettv.launcher", MODE_PRIVATE);
+            SharedPreferences pref = myContext.getSharedPreferences("auth_pref", MODE_MULTI_PROCESS);
+            accessToken = pref.getString("key_result_refresh", "null");
+            Toast.makeText(this, accessToken, Toast.LENGTH_SHORT).show();
+            Log.d("CHeckingAcessToken", accessToken);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
         nettvApplicationComponent = DaggerNettvApplicationComponent.builder().
                 nettvApplicationModule(new NettvApplicationModule(this)).build();
 
@@ -38,7 +52,7 @@ public class NettvApp extends MultiDexApplication{
         OkHttpClient okHttpClient = new OkHttpClient().newBuilder()
                 .connectTimeout(120, TimeUnit.SECONDS)
                 .readTimeout(120, TimeUnit.SECONDS)
-                . writeTimeout(120, TimeUnit.SECONDS)
+                .writeTimeout(120, TimeUnit.SECONDS)
                 .build();
 
         Parse.initialize(new Parse.Configuration.Builder(this)
@@ -48,11 +62,11 @@ public class NettvApp extends MultiDexApplication{
                 .build()
         );
 
-        AndroidNetworking.initialize(getApplicationContext(),okHttpClient);
+        AndroidNetworking.initialize(getApplicationContext(), okHttpClient);
         AndroidNetworking.enableLogging();
     }
 
-    public static void init(){
+    public static void init() {
 
     }
 
