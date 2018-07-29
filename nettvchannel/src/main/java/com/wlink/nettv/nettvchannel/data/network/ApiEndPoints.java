96e3833917a2df204071dbbd78955af084cef2ff
Niroj <niroj.mahrjan@gmail.com>
2018-07-29 15:17:49 +0545

    nimble api changed

--- nettvchannel/src/main/java/com/wlink/nettv/nettvchannel/data/network/ApiEndPoints.java
+++ nettvchannel/src/main/java/com/wlink/nettv/nettvchannel/data/network/ApiEndPoints.java
@@ -4,5 +4,5 @@ public final class ApiEndPoints {
 
     public static final String BASE = "http://api.stage.nettv.com.np/";
     public static final String CHANNEL = BASE +"resellers/worldlink/categories-channels-list";
-    public static final String NIMBLE = BASE +"nimble/wmsAuthSign";
+    public static final String NIMBLE = BASE +"nimble/wmsauthsign";
 }
