f0f64a8bdcb27d796be264a9b070a85cca8b3826
Niroj <niroj.mahrjan@gmail.com>
2018-07-29 11:04:11 +0545

    added token in expection

--- nettvchannel/src/main/java/com/wlink/nettv/nettvchannel/main/NettvApp.java
+++ nettvchannel/src/main/java/com/wlink/nettv/nettvchannel/main/NettvApp.java
@@ -43,6 +43,7 @@ public class NettvApp extends MultiDexApplication {
             Log.d("CHeckingAcessToken", accessToken);
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
+            accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjI1Zjc2OWVhNzRlYjdlM2FmMjg2ZmIyOTc3OGViZjZmNzg2MTA3ZWU2YWU5NjE0NTA5OWZlMDZmNmI2ZGI0NGYyYjg0OGIyY2UxMWIzZDVkIn0.eyJhdWQiOiI4IiwianRpIjoiMjVmNzY5ZWE3NGViN2UzYWYyODZmYjI5Nzc4ZWJmNmY3ODYxMDdlZTZhZTk2MTQ1MDk5ZmUwNmY2YjZkYjQ0ZjJiODQ4YjJjZTExYjNkNWQiLCJpYXQiOjE1MzI4NDEwMjYsIm5iZiI6MTUzMjg0MTAyNiwiZXhwIjoxNTY0Mzc3MDI2LCJzdWIiOiI5Iiwic2NvcGVzIjpbInN1YnNjcmliZXIiXX0.rXPTXvojfGM1hPEzjx2msVOZhPpzOY7cEbMXY29EsBPVVz3ZbuVaN87pTELicsfYpEKGHu_6AYvFGcBNmD15pBr4plEGcw0vpN_Vu_gUXwcNE3AhGoL85BKQZ5WVlGOTl6Tk7IvM9k0Jt4dF4Ee5t7W4qtDciNAWCR12BKjOztsx42b1bSXkxvnkkttqzKAqJO5fElaxiqGYRh3NTMwWlp3DaeGpBeuGDi2cpfbKwvBYrPGV29QeHgB4aHFT5iu-HnTEbC27o2mMjhbZbVfF06gLUjoEWBhcnMrN-q6_0H25Hia7FIIjXHMrI_Bm6y80rtgLhl1CyRndHgQYoMwlIdsbI7ha9uPouGAvqFhdBmEDswW1A0Jg-UlmzDNgD_wRkTWji0ZDDQJJaWR8vryYKCW28-hfxp1k_tNWAepzlIMAt9V3-L9IaU6gQ9-kn_ZWejF5ER2fZCF4OYQCpxfRV6oY5OE0etHa1aVZnwLCk24Zll0b5yW5hhZtX4xlKZf6kSepnoNBzLev_I7cBWYQSZdeuvOjbwaSZPLm35NwIUc_zMXgCP26zLgYG62bTXpk5-PfNS1ipCeztt5uFjADfi841UlLokALEAucx6_7YbZ8k2zkEYkRk35JyprWjgQwToao5Up0CeApP6JOXvgJY3tYrJ2ewiSiSv2C9Cd7gfQ";
             Toast.makeText(this, "error", Toast.LENGTH_SHORT).show();
         }
         nettvApplicationComponent = DaggerNettvApplicationComponent.builder().
