1e61be305a0ee4eba40ef84d06a64a20fe972ce4
Niroj <niroj.mahrjan@gmail.com>
2018-08-01 18:03:18 +0545

    null exception added

--- nettvchannel/src/main/java/com/wlink/nettv/nettvchannel/main/NettvApp.java
+++ nettvchannel/src/main/java/com/wlink/nettv/nettvchannel/main/NettvApp.java
@@ -35,10 +35,12 @@ public class NettvApp extends MultiDexApplication {
     public void onCreate() {
         super.onCreate();
         this.mContext = getApplicationContext();
+        accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjRlZWJjNWY2MjA2NGRmMGUwZmJjYWNiMzdlNDRiNzBiNWVmNjcwYmI2OWI3YWQyMWE3OTZhMzhjMjNkZjA2ZTRhMzMxNTEwZjEwZWNmNmJmIn0.eyJhdWQiOiI4IiwianRpIjoiNGVlYmM1ZjYyMDY0ZGYwZTBmYmNhY2IzN2U0NGI3MGI1ZWY2NzBiYjY5YjdhZDIxYTc5NmEzOGMyM2RmMDZlNGEzMzE1MTBmMTBlY2Y2YmYiLCJpYXQiOjE1MzI4NTc4NjMsIm5iZiI6MTUzMjg1Nzg2MywiZXhwIjoxNTY0MzkzODYzLCJzdWIiOiIzIiwic2NvcGVzIjpbInN1YnNjcmliZXIiXX0.gsuVZCDpefAILkpYGGQcs0EiK5zB0_fh13R5yQcBgxbr4hCECrHMdBV3Gu6llWELB75Py_JRBvercbsihJfXAJdtb6VBds1wbIvfJAT80udcfOhXDjptwdfQ0FLCRDaW_rmGwpVZm6LvJdin4J8BB5MCUG6tuEsqgeG7j4LDSvvyUano9bnbJMRnDx7OW_iJtcqgfxRv1bniBmL_9KVQAVYrlzcQIMWE2sioRoLXeHAzXw7SN9sAqi9rwlc9c-WOcPiPyBIRdyVjSUAtB3g1i0JDBFxe-n6nLLY61YbcwKREIJcpnckl2R_uKiPCc2EkABYlEWeM-1lSfQstr6qp-SbQUIvUAtmpRnEzzcrFqeaQJNgd7hdq3Aryy898P-4p7Uav9u5QMxwds1Gnw2vxyhckqSsO21rZ9k-oJ7qwDv5jeGOhwVtEwyvBXkGecxGWni1yz8pBQJOWCbGG7kWGRo2hTziMduErddKjD8AAYBFacg1Prx7opmhAh6fjtI35VA_Xd5BCEnofFmirU3Huv1OyOMh_x6UQfWD6aBnGbVHGEAtuz_T90d_9D1HWtA_M6NdJEU6ZaO8AWYMZwptmmlbf8BVCjp-_6oIJL8-HmjW3LCc5q1X0VTuUE_4nE_huKzC1iE-JxlSnfAXcY52Maj93Abf4rni1iSFFyMR-x9w";
+
         try {
             Context myContext = createPackageContext("np.com.nettv.launcher", MODE_PRIVATE);
             SharedPreferences pref = myContext.getSharedPreferences("auth_pref", MODE_MULTI_PROCESS);
-            accessToken = pref.getString("key_result_refresh", "null");
+            accessToken = pref.getString("key_result_refresh", accessToken);
             Toast.makeText(this, accessToken, Toast.LENGTH_SHORT).show();
             Log.d("CHeckingAcessToken", accessToken);
         } catch (PackageManager.NameNotFoundException e) {
